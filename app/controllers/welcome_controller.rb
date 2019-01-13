81acdf149d5e75bd419afb3b8ad1828547a61a0b
User <ezzy_nauzzee@hotmail.com>
2019-01-14 03:23:46 +0800

    Added create action in Welcome controller, added new routes in routes.rb and made a login form in root_url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to new_user_path
+    else
+      render 'index'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new'
+  post 'login', to: 'welcome#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
