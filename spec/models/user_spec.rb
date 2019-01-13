0c5fa4060a7385d7a8db8443600a0d4161125914
User <ezzy_nauzzee@hotmail.com>
2019-01-13 20:56:16 +0800

    Rspec test done with User regarding signing up attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(first_name: "John", last_name: "Doe", email: "JohnDoe@hotmail.com", password: "123") }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "it is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+  it "it is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+  it "it is not valid without email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  it "it is not valid without password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
