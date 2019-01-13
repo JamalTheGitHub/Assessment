1b43589843247fdc0d4b6712eeb137dbb91b95b6
User <ezzy_nauzzee@hotmail.com>
2019-01-14 03:40:44 +0800

    Users can log in and log out

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,15 @@ class WelcomeController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      redirect_to new_user_path
+      log_in user
+      redirect_to user
     else
       render 'index'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'login', to: 'welcome#create'
+  delete 'logout', to: 'welcome#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
